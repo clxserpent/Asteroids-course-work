9625ed7ea0afd0eab53d73c368e1688e72fa69d3
clxserpent <130196301+clxserpent@users.noreply.github.com>
2025-01-07 21:12:27 +0000

    deleted redundent code

--- Asteroid.py
+++ Asteroid.py
@@ -3,12 +3,10 @@
 # TODO: scoreboard increases 2 points for medium asteroid 3 points for small asteroid
 # TODO : fix asteroid spawning on player
 # TODO: POWERUPS
-height = 900  # set the height for the 1
+height = 900# set the height for the 1
 width= 600  # sets the width for the display window
 from mainMenu import *
 pygame.init()
-width = 600
-height = 900
 pygame_icon = pygame.image.load("AS.ico") # sets  window icon as icon picture
 pygame.display.set_icon(pygame_icon) # displays icon on window
 display = pygame.display.set_mode((height,width))  # takes the height and weight and uses pygames axis to make a displayd
